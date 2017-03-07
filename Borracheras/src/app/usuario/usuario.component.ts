4ec5324fe63b76d69728c31d31fdacc6f2d038f2
YessLarco <yesselaran@gmail.com>
2017-03-06 19:39:20 -0500

    Componente Usuario
    Crear y listar usuario

--- Borracheras/src/app/usuario/usuario.component.ts
+++ Borracheras/src/app/usuario/usuario.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {Http, Response} from "@angular/http";
 import {MasterUrlService} from "../services/master-url.service";
 import {NgForm} from "@angular/forms";
+import {forEach} from "@angular/router/src/utils/collection";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-usuario',
@@ -12,19 +14,23 @@ export class UsuarioComponent implements OnInit {
   title: string = "Crear Usuario";
   nuevoUsuario: any = {};
   usuarios = [];
-  constructor(private _http: Http,
+  queForm="ListarUsuarios";
+  mostrarCrear=false;
+  constructor(private _ActivatedRoute: ActivatedRoute,private _http: Http,
               private _masterUrl: MasterUrlService) { }
 
   ngOnInit() {
+
     this._http.get(this._masterUrl.url + "Usuario")
       .subscribe(
         (res: Response) => {
           console.log(res.json());
-          this.usuarios = res.json();
-            // .map((value)=>{
-            //   value.formularioCerrado = true;
-            //   return value;
-            // });
+          this.usuarios = res.json()
+            .map((value, key)=>{
+              value.contador = (key+1);
+              value.formularioCerrado = true;
+              return value;
+            });
         },
         (err) => {
           console.log(err);
@@ -33,8 +39,28 @@ export class UsuarioComponent implements OnInit {
     console.log("On Init")
   }
 
-  crarUsuario(formulario:NgForm){
-    
+  crearUsuario(formulario:NgForm){
+    console.log("Estoy en la funcion crear");
+    console.log(formulario);
+    this._http.post(this._masterUrl.url + "Usuario", {
+      nombreUsuario: formulario.value.nombreUsuario,
+      ciudadResidencia: formulario.value.ciudadResidencia,
+      fechaNacimiento: formulario.value.fechaNacimiento
+    })
+      .subscribe((res) => {
+          console.log("No hubo errores");
+          console.log(res);
+          this.usuarios.push(res.json());
+          this.nuevoUsuario = {};
+          //this.disabledButtons.NuevaTiendaFormSubmitButton = false;
+        },
+        (err) => {
+          console.log("Ocurrió un error ", err);
+        },
+        () => {
+          console.log("Termino la funcion vamos a la casa");
+        }
+      );
 
   }
 }
